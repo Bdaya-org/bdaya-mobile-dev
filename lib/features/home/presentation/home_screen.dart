bb13da01f1cd83e8c1419dd0a25c0253aa88daff
RenadEmad <renademad227@gmail.com>
2025-09-05 02:46:28 +0300

    New Commit - Welcome page & fonts setup

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -7,13 +7,111 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home Screen")),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.go('/');
-          },
-          child: const Text("Go to Splash Screen"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Welcome to the Bdaya AI Therapist",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: "Urbanist",
+                  fontSize: 32,
+                  fontWeight: FontWeight.w800,
+                  color: Color(0xff544e86),
+                  letterSpacing: 1.5,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(2, 2),
+                      blurRadius: 4.0,
+                      color: Colors.black26,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Your mindful mental health AI companion for everyone, anywhere 🍃",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: "Urbanist",
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const Spacer(),
+              Image.asset(
+                'assets/images/robot.png',
+                width: 300,
+                height: 300,
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+              ),
+              const Spacer(),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff544e86),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(1000),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                ),
+                onPressed: () {},
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Get Started",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already have an account? ",
+                    style: TextStyle(
+                      fontFamily: "Urbanist",
+                      fontWeight: FontWeight.w300,
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("Sign in tapped!");
+                      // context.go('/login');
+                    },
+                    child: const Text(
+                      "Sign in",
+                      style: TextStyle(
+                        fontFamily: "Urbanist",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Color(0xff517a4a),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
